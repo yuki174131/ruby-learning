95be20f50825da5bad3681973fa7ed5e5392d63e
yuki174131 <[yuki31.messi10@gmail.com]>
2024-08-09 00:52:36 +0900

    繰り返しメソッドのメモ追加

--- プロを目指す人のためのRuby入門/03_配列_繰り返し処理/02_繰り返し.rb
+++ プロを目指す人のためのRuby入門/03_配列_繰り返し処理/02_繰り返し.rb
@@ -21,3 +21,39 @@ numbers.delete_if do |num|
   num.even?
 end
 numbers #=> [1, 3]
+
+numbers = [1, 2, 3, 4]
+sum = 0
+# do...endの代わりに{}で囲んでブロックを作れる
+numbers.each { |num| sum += n }
+sum #=> 10
+
+# 改行を含むブロックを書くときはdo...end
+# 1行でコンパクトに書きたいときは{}のように使い分けられる
+
+# 空の配列を用意して、他の配列をループ処理した結果をからの配列に詰め込んでいくような処理の大半はmapメソッドに置き換えられる
+numbers = [1, 2, 3, 4]
+# ブロックの戻り値が新しい配列の要素になる
+new_numbers = numbers.map { |num| num * 10}
+new_numbers #=> [10, 20, 30, 40]
+
+# ブロックの戻り値がtrueのものだけ集められる。エイリアスメソッドはfind_all
+even_numbers = numbers.select { |num| num.even? }
+even_numbers #=> [2, 4]
+
+# ブロックの戻り値がtrueのものを除外する
+odd_numbers = numbers.reject { |num| num.even? }
+odd_numbers #=> [1, 3]
+
+# ブロックの戻り値が最初にtrueになった要素を返す。エイリアスメソッドはdetect
+min_even_number = numbers.find { |num| num.even? }
+min_even_number #=> 2
+
+# たたみ込み演算を行うメソッド。エイリアスメソッドはreduce
+# injectの引数がresultの初期値。繰り返し処理が終わると、ブロックの戻り値がinjectの戻り値となる
+sum = numbers.inject(0) { |result, num| result += num}
+sum #=> 10
+# 文字列にも使える
+day_of_weeks = ['Mon', 'The', 'Wed', 'Thu', 'Fri', 'Sat'].reduce('Sun') { |result, d| result + d}
+day_of_weeks#=> "SunMonTheWedThuFriSat"
+
